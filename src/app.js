6f348ab692c27e4dd08158a4969d619d84482bf2
Esther López <estherlopez5577@gmail.com>
2023-03-22 18:56:15 -0600

    CRUD medicamento_paciente completo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import pacienteRoute from './routes/pacientes.routes.js';
 import citaRoute from './routes/citas.routes.js';
 import medico_cirugiaRoute from './routes/medico_cirugia.routes.js';
 import examenRoute from './routes/examen.routes.js';
+import medicamento_pacienteRoute from './routes/medicamento_paciente.routes.js';
 
 /** ---------------- 
  * Middlewares
@@ -25,6 +26,7 @@ app.use('/alergia', alergiaRoute);
 app.use('/hospital', hospitalRoute);
 app.use('/medico_cirugia', medico_cirugiaRoute);
 app.use('/examen', examenRoute);
+app.use('/medicamento_paciente', medicamento_pacienteRoute);
 
 app.use((req, res, next)=> {
     res.status(404).json({
