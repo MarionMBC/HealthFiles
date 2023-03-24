67d0c8708ffdb35e00b83d2188aeb6f96ec1d2a6
MarionMBC <melchisedec.bustamante@unah.hn>
2023-03-23 23:24:18 -0600

    Rama de queries

---
+++ HealthFilesQueries/Inserts.sql
@@ -0,0 +1,11 @@
+# Hospital
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-001', 'Hospital Nacional Mario Catarino Rivas');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-002', 'Hospital Escuela Universitario');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-003', 'Instituto Hondureño de Seguridad Social');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-004', 'Hospital Regional del Norte');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-005', 'Hospital Regional del Sur');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-006', 'Hospital Regional Atlántida');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-007', 'Hospital Leonardo Martinez');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-008', 'Hospital Materno Infantil');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-009', 'Hospital de Occidente');
+INSERT INTO healthfiles.hospital (codigo_hospital, nombre_hospital) VALUES ('HN-HOSP-010', 'Hospital San Francisco');
