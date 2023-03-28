c3d0b95a1675526809070811f613d71d359e73c3
MarionMBC <melchisedec.bustamante@unah.hn>
2023-03-27 21:02:09 -0600

    Pruebas con variables de entorno

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { SERVER_PORT } from "./env-config.js";
 /** 
  ** Inicializa el servidor
 */
-app.listen( 8993 |4000|SERVER_PORT, ()=>{
+app.listen( 4000|SERVER_PORT, ()=>{
     console.log("Servidor inicializado en el puerto", SERVER_PORT);
 });
 
