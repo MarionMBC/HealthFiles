f477ea02ad66a35da86a7e09af4cc1976fe77b58
Esther López <estherlopez5577@gmail.com>
2023-03-29 19:35:54 -0600

    Campos completos de formulario de registro de citas

--- Frontend/HealthFiles/screens/RegistroCitaScreen.jsx
+++ Frontend/HealthFiles/screens/RegistroCitaScreen.jsx
@@ -1,14 +1,24 @@
+/**
+ * Author: Jennebier Esther Alvarado López
+ * Date: 29/03/2023
+ * Description: Pantalla para el registro de una cita para el usuario doctor
+ */
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import { View, Text, TextInput, Button, ScrollView } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { Calendar } from 'react-native-calendars';
+import PickerComp from '../components/PickerComp.jsx';
 import styles from '../styles/styles.js';
-export default function RegistroCitaScreen() {
-  const [name, setName] = useState('');
+export default function RegistroCitaScreen({navigation}) {
+  
+  //Declaración de variables de estado
   const [motivo, setMotivo] = useState('');
-  const [selectedDate, setSelectedDate] = useState(' ');
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+  const [selectedHospital, setSelectedHospital] = useState('');
+  const [selectedHora, setSelectedHora] = useState('');//Para mantener este como valor seleccionado
 
 
+  //Handles
   const handleMotivoChange = (text) => {
     setMotivo(text);
   };
@@ -17,50 +27,82 @@ export default function RegistroCitaScreen() {
     setSelectedDate(date.dateString);
   };
 
-  const handleOptionSelect = (itemValue) => {
-    setSelectedOption(itemValue);
+  const handleHospitalSelect = (hospital) => {
+    setSelectedHospital(hospital);
+  };
+
+  const handleHoraSelect = (hora) => {
+    setSelectedHora(hora);
   };
 
   const handleSubmit = () => {
-    console.log(`Submitted:  ${email}, ${selectedDate}`);
+    console.log(`Submitted:  ${motivo}, ${selectedDate}`);
   };
 
-  const [selectedValue, setSelectedValue] = useState("Escuela");
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+  const handleConfirm = () => {
+    hideDatePicker();
+  };
+  
 
   return (
+    <ScrollView>
     <View style={styles.container}>
       <Text style={styles.title}>Agendar una cita</Text>
-      <Text>Hospital</Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Escuela" value="Escuela" />
-        <Picker.Item label="Viera" value="Viera" />
-      </Picker>
       
+      <Text style={styles.label}>Hospital</Text>
+      <PickerComp style={styles.selector} selectedValue={selectedHospital} setSelectedValue={handleHospitalSelect} />
+      
+      <Text style={styles.label}>Fecha de la cita</Text>
+      <Calendar
+        onDayPress={handleDateSelect}
+      />
+    
+      <Text style={styles.label}>Hora de la consulta</Text>
+      <PickerComp selectedValue={selectedHora} setSelectedValue={handleHoraSelect} />
+
+    <Text style={styles.label}>Motivo</Text>
       <TextInput
-      multiline
-      maxLength={30}
+        multiline={true}
+        maxLength={30}
         style={styles.input}
         placeholder="Motivo"
         onChangeText={handleMotivoChange}
         value={motivo}
       />
-       <Calendar
-        onDayPress={handleDateSelect}
+      
+      <Text style={styles.label}>Hora para el recordatorio de la cita</Text>
+      <DateTimePickerModal
+      isVisible={datePickerVisible}
+      mode="time"
+      onConfirm={handleConfirm}
+      onCancel={hideDatePicker}
       />
+      <Button 
+      title="Establecer recordatorio" 
+      style={styles.buttonForm}
+      onPress={showDatePicker} />
+
       <Button
-        title="Establecer recordatorio"
-        
-      />
-      <Button
+        style={styles.buttonForm}
         title="Agendar"
         onPress={handleSubmit}
       />
+
+      <Button
+        style={styles.buttonForm}
+        title="Cancelar"
+        onPress={() => navigation.navigate('Home')}
+      />
+
     </View>
+    </ScrollView>
   );
 }
-
-
--- Frontend/HealthFiles/styles/styles.js
+++ Frontend/HealthFiles/styles/styles.js
@@ -2,21 +2,39 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal:40,
+    paddingVertical:30,
   },
   title: {
     fontSize: 24,
     marginBottom: 10,
+    fontWeight: 'bold',
+    backgroundColor:'#40E0D0',
+    color:'white',
+  },
+  label:{
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: 'regular',
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderRadius:10,
+    borderColor: '#9D9D9D',
     borderWidth: 1,
     marginBottom: 10,
     padding: 5,
     width: 250,
+  },
+  selector:{
+    backgroundColor: 'D9D9D9',
+    borderColor: '9D9D9D',
+    borderWidth:1,
+    overflow:'viseble',
+  },
+  buttonForm:{
+    borderRadius:30,
+    marginVertical:10,
   },
 });
 
