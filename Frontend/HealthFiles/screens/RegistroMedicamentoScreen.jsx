15333f6de93f0304811ce5241511c8c60e2692fd
MarionMBC <melchisedec.bustamante@unah.hn>
2023-04-02 14:06:59 -0600

    Creación de pantalla de medicamentos v.1.2

--- Frontend/HealthFiles/screens/RegistroMedicamentoScreen.jsx
+++ Frontend/HealthFiles/screens/RegistroMedicamentoScreen.jsx
@@ -1,13 +1,16 @@
 import {ScrollView, Text, View} from "react-native";
 import styles from "../styles/styles";
 import SearchBarComp from "../components/SearchBar";
+import {Divider} from "@rneui/base";
 
 const RegistroMedicamentoScreen = ({navigation})  => {
     return (
         <ScrollView>
             <SearchBarComp>
             </SearchBarComp>
+
             <View style={styles.container}>
+                <Divider></Divider>
             </View>
         </ScrollView>
     )
