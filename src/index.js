d131ef279ff5db857f2fdcb342b5da4e310dcd39
MarionMBC <melchisedec.bustamante@unah.hn>
2023-04-08 21:36:07 -0600

    Pruebas con backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import app from "./app.js";
 import { SERVER_PORT } from "./env-config.js";
-
+import express from "express";
 
 /** 
  ** Inicializa el servidor
 */
+
+app.use(express.json());
+
 
 app.get('/', (req, res)=> {
     res.send('hola');
