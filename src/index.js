60d138fbe4630f060d27ba3045ac3607c69dcb5d
MarionMBC <melchisedec.bustamante@unah.hn>
2023-04-08 22:47:33 -0600

    Pruebas con backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { SERVER_PORT } from "./env-config.js";
 import express from "express";
-
+const PORT = 3000
 /** 
  ** Inicializa el servidor
 */
@@ -14,8 +14,8 @@ app.get('/', (req, res)=> {
     res.send('hola');
 })
 
-app.listen( 4000, ()=>{
-    console.log("Servidor inicializado en el puerto", 4000);
+app.listen( PORT, ()=>{
+    console.log("Servidor inicializado en el puerto", PORT);
 });
 
 
