68aee917332eba7c1bf3df682b05148c606ac9df
Rony Josue Gomez <ronyjosuegomez@gmail,com>
2023-04-16 18:03:15 -0600

    Cambios realizados en LoginHelper y SignUpScreen

--- Frontend/HealthFiles/helpers/Login.helper.jsx
+++ Frontend/HealthFiles/helpers/Login.helper.jsx
@@ -9,3 +9,22 @@ export const obtenerCorreoContrasena = async (correo_electronico='juan.perez@gma
             return e
     }
 }
+
+const URL = "https://healthfiles-production.up.railway.app/";
+
+export const GuardarPaciente = async (data) =>{
+     const urlCita = `${URL}paciente/create`
+     await fetch(urlCita, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
