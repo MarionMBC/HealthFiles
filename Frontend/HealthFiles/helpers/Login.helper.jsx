0f8c531c078b318d4fa1674bbc4347cd3c7dad4c
Rony Josue Gomez <ronyjosuegomez@gmail,com>
2023-04-23 13:12:26 -0600

    Cambios realizados en URL de login helper

--- Frontend/HealthFiles/helpers/Login.helper.jsx
+++ Frontend/HealthFiles/helpers/Login.helper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const obtenerCorreoContrasena = async (correo_electronico = 'juan.perez@gmail.com', contrasena = 'password123') => {
   try {
-    const response = await fetch(`https:/healthfiles-production.up.railway.app/paciente/get/${correo_electronico}/${contrasena}`)
+    const response = await fetch(`https://healthfiles.azurewebsites.net/paciente/get/${correo_electronico}/${contrasena}`)
     const data = await response.json()
     return data
   } catch (e) {
